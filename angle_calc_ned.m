4099a5b6fe522b0b798081b4b0243fc475133e14
DaryaMalafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2019-08-09 13:11:49 +0300

    Delete angle_calc_ned.m

--- angle_calc_ned.m
+++
@@ -1,9 +0,0 @@
-
-function RPY_new = angle_calc_ned(ya_rpy)
-%RPY_n = zeros();
- % расчёт углов по измерениям акселерометров с погрешностями
-  RPY_new(1) = atan2(ya_rpy(2), ya_rpy(3));
-  RPY_new(2) = atan2(-(ya_rpy(1)), sqrt(((ya_rpy(2)^2)+(ya_rpy(3))^2)));
-
-  RPY_new(3) = 0;
-end
