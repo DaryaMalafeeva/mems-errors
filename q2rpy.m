f1de9cb28204f05c846f4588522a9eb35b344e61
DaryaMalafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2019-08-16 17:37:35 +0300

    Delete q2rpy.m

--- q2rpy.m
+++
@@ -1,6 +0,0 @@
-function RPY = q2rpy(Q) % quaternion to euler angles conversion  (c)wikipedia
- RPY=zeros(3,1);
- RPY(1) = atan2( 2.0*(Q(1)*Q(2)+Q(3)*Q(4)), (1.0-2.0*(Q(2)*Q(2)+Q(3)*Q(3)) ) );
- RPY(2) = asin( 2.0*(Q(1)*Q(3)-Q(2)*Q(4))  );
- RPY(3) = atan2( 2.0*(Q(1)*Q(4)+Q(2)*Q(3)), (1.0-2.0*(Q(3)*Q(3)+Q(4)*Q(4)) ) );
-  
