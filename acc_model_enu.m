821f7116c2f33318fca55e04fcb14a72d2978213
Darya Malafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2019-11-08 17:48:29 +0300

    все ок но вопросы к параметрам магнитометра

--- acc_model_enu.m
+++ acc_model_enu.m
@@ -1,129 +1,128 @@
 clear all; close all; clc
+
 % number of points
 N = 2000; 
 
 % memory allocation
-C_rpy_ned    = zeros(3,3);
-ya_rpy       = zeros(3,N);
-RPY_new      = zeros(N,3);
-err_RPY      = zeros(N,3);
-M_rpy        = zeros(3,N);
-A_rpy        = zeros(3,N);
-ym_rpy       = zeros(3,N);
-ym_rp0       = zeros();
+C_rpy_ned = zeros(3,3);
+ya_rpy = zeros(3,N);
+RPY_new = zeros(N,3);
+err_RPY = zeros(N,3);
+M_rpy = zeros(3,N);
+A_rpy = zeros(3,N);
+ym_rpy = zeros(3,N);
+ym_rp0 = zeros();
 Azimuth_magn = zeros(1,N);
-err_Azimuth  = zeros(1,N);
-Azimuth      = zeros(1,N);
-ym_enu       = zeros(3,N);
+err_Azimuth = zeros(1,N);
+Azimuth = zeros(1,N);
+ym_enu = zeros(3,N);
 
 % true roll, pitch, yaw angles
 % random limited angles 
-R = (rand(N,1)*2*pi - pi)* 165/180;  
+R = (rand(N,1)*2*pi - pi)* 165/180;
 
 % constant angles
-%R = ones(N,1)*deg2rad(180);
+%R = ones(N,1)*deg2rad(180);  
 
- % zero angles
-%R = zeros(N,1);                   
+% zero angles
+%R = zeros(N,1);                    
 
 P = (rand(N,1)*pi - pi/2)* 75/90; 
 %P = ones(N,1)*deg2rad(90);
 %P = zeros(N,1);
 
-Y = (rand(N,1)*2*pi - pi)* 165/180; 
+%Y = (rand(N,1)*2*pi - pi)* 165/180; 
 %Y = ones(N,1)*deg2rad(180);
-%Y = zeros(N,1);
+Y = zeros(N,1);
 
 % angles matrix
-RPY = [R P Y]; 
+RPY = [R P Y];            
 
 % vector of the true value of the magnetic field 
-M_enu = [0; 11; -8] * 1e-6 * 1; 
+M_enu = [0; 11; -8] * 1e-6 * 1;  
 
-
-%IMU PARAMETERS
+% IMU PARAMETERS
 
 % MAGNETOMETER
 
-% initial bias error - ???????? ????? ???????????? from mgauss to tesla
-b_m = [15e-3; 15e-3; 15e-3] * 1e-4;
- 
-% misalignment - axis to frame - ???????? ???? ????????????
-mis_m   = deg2rad([0 -1 1; -1 0 1; 1 -1 0]) ; 
+% 
+mis_m = eye(3,3);
 
-% initial sensitivity tolerance - ?????????? ??????????? ????????????
-m_m = diag([ 0.02; 0.02; 0.02]) * 1; % in %
+% 
+m_m = diag([ 1/10; 1/10; 1/10]);
+
+%
+b_m = [1 ; 1; 1]* 1e-6 * 1;
 
 % output noise
-Density_m = 0.042 * 1e-6; % spectral density
 
-BW        = 218.1; % bandwidth
-
-sigma_m   = Density_m*sqrt(BW) * 1;
-
+sigma_m = 0.6 * 1e-6 * 1;
 
 % ACCELEROMETER
 
-% bias repeatability - ñìåùåíèå íóëåé
-b_a     = [0.016; -0.016; 0.016] * 1;
+% zero-g initial calibration tolerance (bias)
+b_a = [0.06; -0.06; 0.08];
 
-% axis to axis misalignment - ïåðåêîñû îñåé 
-mis_a   = deg2rad([0 -1e-3 0.035; -0.035 0 0.035; 0.35 -0.035 0]); 
+% cross axis sensitivity
+mis_a = [0 -0.002 0.002; -0.002 0 0.002; 0.002 -0.002 0] * 1;
 
-% repeatability - ìàñøàòáíûå êîýôôèöèåíòû
-m_a     = diag([-0.005; 0.005; -0.005]) * 1; % in %
+% initial tolerance
+m_a = diag([-0.03; 0.03; -0.03]) * 1;
 
-% output noise
+% output noise 
 
-Density_a = 300 * 1e-6;
+Density_a = 300 * 1e-6; % noise power spectral density
 
-BW        = 218.1;
+BW = 218.1; % bandwidth
 
-sigma_a   = Density_a * sqrt(BW) * 1;
-        
+sigma_a = Density_a *sqrt(BW) * 1; 
+
+
 acc_parameters  = struct('b_a', b_a, 'mis_a', mis_a, 'm_a', m_a, 'sigma_a', sigma_a);
 
-magn_parameters = struct('b_m', b_m, 'mis_m', mis_m,'m_m', m_m,'sigma_m', sigma_m);
+magn_parameters = struct('b_m', b_m, 'mis_m', mis_m, 'm_m', m_m, 'sigma_m', sigma_m);
 
 % calculations
 for i = 1:N
   % true angles to quaternion convertion
-  Quat_rpy_enu = rpy2q(RPY(i,1:3)'); 
+  Quat_rpy_enu = rpy2q(RPY(i,1:3)');        
   
   % quaternion to matrix convertion
-  C_rpy_enu(1:3,1:3) = q2mat(Quat_rpy_enu); 
+  C_rpy_enu(1:3,1:3) = q2mat(Quat_rpy_enu);               
   
   % true acceleration vector
-  A_rpy(1:3,i) = C_rpy_enu(1:3,1:3)' * [0; 0; -1]; 
+  A_rpy(1:3,i) = C_rpy_enu(1:3,1:3)' *[0; 0; -1];        
   
   % true magnetic field vector
-  M_rpy(1:3,i) = C_rpy_enu(1:3,1:3)' * M_enu;     
+  M_rpy(1:3,i) = C_rpy_enu(1:3,1:3)' * M_enu;             
   
   % true azimuth angle
-  Azimuth(i) = Y(i);                              
+  Azimuth(i) = Y(i);                                      
   
   % accelerometer error model application 
   ya_rpy(1:3,i)= acc_meas(A_rpy(1:3,i), acc_parameters); 
   
-  % magnetometer error model application
-  ym_rpy(1:3,i) = m_meas(M_rpy(1:3,i), magn_parameters);  
+  % magnetometer error model application 
+  ym_rpy(1:3,i) = m_meas(M_rpy(1:3,i), magn_parameters); 
   
-  % angles calculating by true accelaration vector
-  %RPY_new1 = angle_calc(A_rpy(1:3,i)); 
+  % angles calculating by true accelaration vector 
+  %RPY_new1 = angle_calc(A_rpy(1:3,i));  
   
-  % angles calculating by accelerometer measurements 
-  RPY_new1 = angle_calc(ya_rpy(1:3,i));
-  RPY_new(i,1:3)= RPY_new1;
+  % angles calculating by accelerometer measurements
+  RPY_new1 = angle_calc(ya_rpy(1:3,i));                    
+  RPY_new(i,1:3)= RPY_new1; 
   
   % debug
   % RPY_new1 = RPY(i,1:3);
-  % RPY_new(i,1:3)= RPY(i,1:3);
+  %RPY_new(i,1:3)= RPY(i,1:3);
 
   %C_rpy_enu(1:3,1:3) = rpy2mat([R(i); P(i); 0]);
   %C_rpy_enu(1:3,1:3) = rpy2mat([RPY_new1(1:2)'; 0]);
   
   % measured angles to quaternion convertion
   Quat_rpy_enu_new = rpy2q([RPY_new1(1:2)'; 0]); 
+  
+  % q2m convertion 
   C_rpy_enu_new(1:3,1:3) = q2mat(Quat_rpy_enu_new); 
   
   % magnetometer measurements convertion from body frame coordinates to LCS
@@ -131,10 +130,10 @@ for i = 1:N
   %ym_enu(1:3,i) = C_rpy_enu_new(1:3,1:3) * M_rpy(1:3,i);
   
   % azimuth angle calculation
-  Azimuth_magn(i) = atan2(ym_enu(1,i),ym_enu(2,i)); 
+  Azimuth_magn(i) = atan2(ym_enu(1,i),ym_enu(2,i));       
  
   % angles errors calculation
-  err_RPY(i,1:3) = RPY(i,1:3) - RPY_new(i,1:3); 
+  err_RPY(i,1:3) = RPY(i,1:3) - RPY_new(i,1:3);           
   err_Azimuth(i) = Azimuth_magn(i) -  Azimuth(i); 
   
   % angles correction 
@@ -162,11 +161,11 @@ plot3(R*180/pi, P*180/pi, err_PPY_deg(:,1), '.')
 ax = gca;
 set(ax,'xtick',(-180:90:180));
 set(ax,'ytick',(-90:30:90));
-title('roll error vs roll & pitch')
+title('?????? ????? ?? ????? ? ???????')
 grid on
-xlabel('roll, deg')
-ylabel('pitch, deg')
-zlabel('roll error, deg')
+xlabel('????, ????')
+ylabel('??????, ????')
+zlabel('r?????? ?????, ????')
 grid on
 
 % pitch error vs roll & pitch
@@ -175,15 +174,15 @@ plot3(R*180/pi, P*180/pi, err_PPY_deg(:,2), '.')
 ax = gca;
 set(ax,'xtick',(-180:90:180));
 set(ax,'ytick',(-90:90:90));
-title('pitch error vs roll & pitch')
-xlabel('roll, deg')
-ylabel('pitch, deg')
-zlabel('pitch error, deg')
+title('?????? ??????? ?? ????? ? ???????')
+xlabel('????, ????')
+ylabel('??????, ????')
+zlabel('?????? ???????, ????')
 grid on
 
-% % to plot the following dependencies correctly you must set one of the
-% % angles by zeros 
-% 
+% to plot the following dependencies correctly you must set one of the
+% angles by zeros 
+
 % % azimuth error vs roll & pitch
 % figure
 % plot3(R*180/pi, P*180/pi, err_Azimuth_deg, '.')
