eac486efe57d126deb37e49c930ecc9f6fcee30f
Darya Malafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2019-11-08 12:40:03 +0300

    Update m_meas.m
    правки функции

--- m_meas.m
+++ m_meas.m
@@ -1,3 +1,4 @@
 function ym_rpy = m_meas(M_rpy,magn_parameters)
- ym_rpy = magn_parameters.s_m * M_rpy(1:3,1) + magn_parameters.b_m  + magn_parameters.sigma_m*randn(3,1);
+ ym_rpy = (eye(3,3) + magn_parameters.m_m + magn_parameters.mis_m) * M_rpy(1:3,1) + magn_parameters.b_m  + magn_parameters.sigma_m*randn(3,1);
 end
+
