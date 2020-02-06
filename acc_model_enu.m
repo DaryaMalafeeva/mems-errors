8d119cbb8169a4ebd2ca844a431ef9327bc28b2a
naginia <naginia@yandex.ru>
2020-02-06 18:55:46 +0300

    Убил вкравшийся в расчет магнитного азимута баг. Надо перестроить и перезалить графики на хабрасторейдж.
    Можно заодно почистить код от дебага, оставив только необходимое для статьи.

--- acc_model_enu.m
+++ acc_model_enu.m
@@ -41,8 +41,8 @@ A_enu = [0; 0; -1];  % g
 
 %--------------------------------Choose IMU--------------------------------
 
-%[acc_parameters, magn_parameters, imu_name] = get_MPU9250A_parameters();
-[acc_parameters, magn_parameters, imu_name] = get_ADIS16488A_parameters();
+[acc_parameters, magn_parameters, imu_name] = get_MPU9250A_parameters();
+%[acc_parameters, magn_parameters, imu_name] = get_ADIS16488A_parameters();
 %--------------------------------------------------------------------------
 
 % calculations
@@ -62,16 +62,18 @@ for i = 1:N
   
   % accelerometer error model application 
   ya_rpy(1:3,i)= acc_meas(A_rpy(1:3,i), acc_parameters); 
+%    ya_rpy(1:3,i) = A_rpy(1:3,i); % using IDEAL accelerometers
   
   % magnetometer error model application 
   ym_rpy(1:3,i) = m_meas(M_rpy(1:3,i), magn_parameters); 
-  
+%    ym_rpy(1:3,i) = M_rpy(1:3,i); % using IDEAL magnetometers
+
   % angles calculating by true accelaration vector 
   %RPY_new1 = angle_calc(A_rpy(1:3,i));  
   
   % angles calculating by accelerometer measurements
-  RPY_new = angle_calc(ya_rpy(1:3,i));                    
-  RPY_new(i,1:3)= RPY_new; 
+  RPY_new__ = angle_calc(ya_rpy(1:3,i));                    
+  RPY_new(i,1:3)= RPY_new__; 
   
   % debug
   % RPY_new1 = RPY(0000FFi,1:3);
@@ -86,7 +88,7 @@ for i = 1:N
 % % %   % q2m convertion 
 % % %   C_rpy_enu_new(1:3,1:3) = q2mat(Quat_rpy_enu_new); 
 % % %   
-  C_rpy_enu_new(1:3,1:3) = rpy2mat([RPY_new(1:2)'; 0]); % magn to horizontal plane
+  C_rpy_enu_new(1:3,1:3) = rpy2mat([RPY_new(i,1:2)'; 0]); % magn to horizontal plane
   
   % magnetometer measurements convertion from body frame coordinates to horizontal
   ym_enu(1:3,i) = C_rpy_enu_new(1:3,1:3) * ym_rpy(1:3,i); 
@@ -119,37 +121,37 @@ err_Azimuth_deg = err_Azimuth*180/pi;
 % plotting
 % roll error vs roll & pitch
 
-% figure(1);
-% plot3(R*180/pi, P*180/pi, err_PPY_deg(:,1), '.r')
-% ax = gca;
-% set(ax,'xtick',(-180:90:180));
-% set(ax,'ytick',(-90:30:90));
-% set(ax,'ztick',(-30:10:30));
-% %legend(['roll error vs roll & pitch for ' imu_name])
-% title(['roll error vs roll & pitch for ' imu_name])
-% grid on
-% xlabel('roll, deg')
-% ylabel('pitch, deg')
-% zlabel('roll error, deg')
-% grid on
-% cmnstr = 'r_err_vs_r_p_err.png';
-% print(figure(1), cmnstr, '-dpng', '-r300');
+figure(1);
+plot3(R*180/pi, P*180/pi, err_PPY_deg(:,1), '.r')
+ax = gca;
+set(ax,'xtick',(-180:90:180));
+set(ax,'ytick',(-90:30:90));
+set(ax,'ztick',(-30:10:30));
+%legend(['roll error vs roll & pitch for ' imu_name])
+title(['roll error vs roll & pitch for ' imu_name])
+grid on
+xlabel('roll, deg')
+ylabel('pitch, deg')
+zlabel('roll error, deg')
+grid on
+cmnstr = 'r_err_vs_r_p_err.png';
+print(figure(1), cmnstr, '-dpng', '-r300');
 % 
 % 
 % % pitch error vs roll & pitch
-% figure(2);
-% plot3(R*180/pi, P*180/pi, err_PPY_deg(:,2),'.r')
-% ax = gca;
-% set(ax,'xtick',(-180:90:180));
-% set(ax,'ytick',(-90:90:90));
-% set(ax,'ztick',(-10:2:10));
-% title(['pitch error vs roll & pitch for ' imu_name])
-% xlabel('roll, deg')
-% ylabel('pitch, deg')
-% zlabel('pitch error, deg')
-% grid on
-% cmnstr = 'p_err_vs_r_p_err.png';
-% print(figure(2), cmnstr, '-dpng', '-r300');
+figure(2);
+plot3(R*180/pi, P*180/pi, err_PPY_deg(:,2),'.r')
+ax = gca;
+set(ax,'xtick',(-180:90:180));
+set(ax,'ytick',(-90:90:90));
+set(ax,'ztick',(-10:2:10));
+title(['pitch error vs roll & pitch for ' imu_name])
+xlabel('roll, deg')
+ylabel('pitch, deg')
+zlabel('pitch error, deg')
+grid on
+cmnstr = 'p_err_vs_r_p_err.png';
+print(figure(2), cmnstr, '-dpng', '-r300');
              
 % to plot the following dependencies correctly you must set one of the
 % angles by zeros 
